04bf70a33539261bc9620fba39602eae0dc8d2a0
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-01-24 13:25:34 -0500

    Added routing for paramters

--- index.php
+++ index.php
@@ -13,8 +13,29 @@ $f3 = Base::Instance();
 $f3->route("GET /", function (){
     echo "<h1>My Pets</h1>";
     echo "<a href='order'>Order a Pet</a>";
-    //$view = new Template();
-    //echo $view->render("views/home.html");
+});
+
+$f3->route("GET /@animal", function($f3, $params) {
+    $animal = $params['animal'];
+    switch ($animal) {
+        case 'chicken':
+            echo "Cluck!";
+            break;
+        case 'dog':
+            echo "Woof!";
+            break;
+        case 'cat':
+            echo "Meow!";
+            break;
+        case 'horse':
+            echo "Nay!";
+            break;
+        case 'cow':
+            echo "Moo!";
+            break;
+        default:
+            $f3->error(404);
+    }
 });
 
 //Run f3
